ea9c95f3e8b637b439e46961e333650d69c89289
benny <bengaminn@ravtech.co.il>
2021-11-16 17:34:44 +0200

    first

--- stars/ViewController.swift
+++ stars/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum starStatus {
+        case full
+        case empty
+    }
+    
+    var allStarsStatus : [Int:starStatus] = [:]
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for star in stars {
+            allStarsStatus = [star.tag: starStatus.empty]
+        }
     }
-
-
+   
+    @IBOutlet var stars: [UIButton]!
+    
+    @IBAction func changeStarStatus(_ sender: UIButton) {
+       
+        if allStarsStatus[sender.tag] == .empty {
+            sender.setImage(UIImage(systemName:  "star.fill"), for: UIControl.State.normal)
+            allStarsStatus[sender.tag] = .full        }
+        else{
+            sender.setImage(UIImage(systemName:  "star"), for: UIControl.State.normal)
+            allStarsStatus[sender.tag] = .empty        }
+        
+        
+        
+       
+        
+        
+    }
 }
 
